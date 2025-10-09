90ed9ccf6133859aefc0f3924cb02bfe32033f2f
Rod-97 <200969810+Rod-97@users.noreply.github.com>
2025-10-09 16:28:15 +0200

    carousel logic done

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const closeIcon = document.querySelector(".close-icon");
-const mobileNav = document.querySelector(".mobile-nav");
-const shadow = document.querySelector(".shadow");
+handleNavbarEvents();
+handleDisplayedProductEvents();
 
-hamburgerIcon.addEventListener("click", () => {
-  mobileNav.classList.add("active");
-  shadow.classList.add("active");
-});
+function handleNavbarEvents() {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const closeIcon = document.querySelector(".close-icon");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const shadow = document.querySelector(".shadow");
 
-[closeIcon, shadow].forEach((el) => {
-  el.addEventListener("click", () => {
-    mobileNav.classList.remove("active");
-    shadow.classList.remove("active");
+  hamburgerIcon.addEventListener("click", () => {
+    mobileNav.classList.add("active");
+    shadow.classList.add("active");
   });
-});
+
+  [closeIcon, shadow].forEach((el) => {
+    el.addEventListener("click", () => {
+      mobileNav.classList.remove("active");
+      shadow.classList.remove("active");
+    });
+  });
+}
+
+function handleDisplayedProductEvents() {
+  const prevIcon = document.querySelector(".prev-icon");
+  const nextIcon = document.querySelector(".next-icon");
+  const productImages = [...document.querySelectorAll(".product-img")];
+
+  prevIcon.addEventListener("click", () => {
+    const selectedProduct = productImages.find((productImg) =>
+      productImg.classList.contains("selected")
+    );
+
+    const selectedProductIdx = Number(selectedProduct.dataset.number);
+
+    if (selectedProductIdx === 0) return;
+
+    productImages[selectedProductIdx].classList.remove("selected");
+
+    const newProductIdx = selectedProductIdx - 1;
+
+    productImages[newProductIdx].classList.add("selected");
+  });
+
+  nextIcon.addEventListener("click", () => {
+    const selectedProduct = productImages.find((productImg) =>
+      productImg.classList.contains("selected")
+    );
+
+    const selectedProductIdx = Number(selectedProduct.dataset.number);
+
+    if (selectedProductIdx === productImages.length - 1) return;
+
+    productImages[selectedProductIdx].classList.remove("selected");
+
+    const newProductIdx = selectedProductIdx + 1;
+
+    productImages[newProductIdx].classList.add("selected");
+  });
+}
